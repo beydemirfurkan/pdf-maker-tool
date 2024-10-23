2a527bc0372ccb92218ecc8b3f2589698bb4dadd
Furkan Beydemir <furkanbeydemirr@gmail.com>
2024-10-23 09:23:54 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import Vue3Resize from 'vue3-resize'
+import 'vue3-resize/dist/vue3-resize.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Vue3Resize)
+app.mount('#app')
